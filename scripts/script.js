ec881ebe65a734be7d53b795720bcb9cf4f852bb
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-10-22 00:41:36 +0600

    fix: update language attribute and format last modified date

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const lastModified = document.getElementById("last-modified");
-  lastModified.textContent = `Last Modified: ${document.lastModified}`;
+  const date = new Date(document.lastModified);
+  lastModified.innerText = `Last Modified: ${date.toLocaleDateString(
+    "en-GB"
+  )} ${date.toLocaleTimeString("en-GB")}`;
 });
